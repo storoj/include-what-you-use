b39b4a8cdc4c04aae8664bfe4dd32734c0f8a43d
Volodymyr Sapsai <vsapsai@gmail.com>
2011-08-04 00:21:25 +0300

    Added test if existing @class forward declaration is taken into account

---
+++ tests/fwd_decl_class_as_ivar_already.h
@@ -0,0 +1,28 @@
+//===--- fwd_decl_class_as_ivar_already.h - test input file for iwyu ------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+// Tests that forward declaration is respected. Actually it is
+// fwd_decl_class_as_ivar.h, .cc with applied IWYU recommendations. Obviously
+// IWYU should not offer more changes.
+
+#import <objc/Object.h>
+
+@class ImportedClass;
+
+@interface C : Object {
+ @private
+  ImportedClass *var;
+}
+@end
+
+/**** IWYU_SUMMARY
+
+(tests/fwd_decl_class_as_ivar_already.h has correct #includes/fwd-decls)
+
+***** IWYU_SUMMARY */
